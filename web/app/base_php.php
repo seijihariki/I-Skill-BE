668fac2ca2d7ca5f456022542b1e8de5a06aef08
Victor Seiji Hariki <victorseijih@gmail.com>
2016-10-23 16:15:18 -0200

    Fixed passwords

--- web/app/base_php.php
+++ web/app/base_php.php
@@ -2,7 +2,7 @@
 require_once('../../vendor/autoload.php');
 
 $dbhost = 'ec2-54-221-253-87.compute-1.amazonaws.comOC';
-$dbport = 5432;
+$dbport = '5432';
 $dbname = 'db2bq0vfsn68vn';
 $dbuser = 'odfehicdceyspg';
 $dppass = ' GA70wAn9eSONOLUVk9Iihs04U5';
--- web/app/login.php
+++ web/app/login.php
@@ -2,10 +2,10 @@
 require_once('../../vendor/autoload.php');
 
 $dbhost = 'ec2-54-221-253-87.compute-1.amazonaws.comOC';
-$dbport = 5432;
+$dbport = '5432';
 $dbname = 'db2bq0vfsn68vn';
 $dbuser = 'odfehicdceyspg';
-$dppass = ' GA70wAn9eSONOLUVk9Iihs04U5';
+$dppass = 'GA70wAn9eSONOLUVk9Iihs04U5';
 
 $dbconn = pg_connect("host=".$dbhost." port=".$dbport." dbname=".$dbname." user=".$dbuser." password=".$dbpass);
 
