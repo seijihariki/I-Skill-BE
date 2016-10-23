231e7538df6ab59c627e1b6fb03d83912dba3cde
Victor Seiji Hariki <victorseijih@gmail.com>
2016-10-23 16:29:24 -0200

    Corrected DB auth creds

--- web/app/base_php.php
+++ web/app/base_php.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../../vendor/autoload.php');
 
-$dbhost = 'ec2-54-221-253-87.compute-1.amazonaws.comOC';
+$dbhost = 'ec2-54-221-253-87.compute-1.amazonaws.com';
 $dbport = '5432';
 $dbname = 'db2bq0vfsn68vn';
 $dbuser = 'odfehicdceyspg';
-$dppass = ' GA70wAn9eSONOLUVk9Iihs04U5';
+$dbpass = 'GA70wAn9eSONOLUVk9Iihs04U5';
 
 $dbconn = pg_connect("host=".$dbhost." port=".$dbport." dbname=".$dbname." user=".$dbuser." password=".$dbpass);
 
